3cc4bcb572d512ef55dd85c1d165e5746d35226e
reggiegunawan <reggiegunawan88@gmail.com>
2021-11-13 21:31:07 +0700

    feat(icon): update website title and icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: 'cs-initial.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
