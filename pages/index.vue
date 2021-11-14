ff7d1c383114d9bcc562254b9f249e75a7521773
reggiegunawan <reggiegunawan88@gmail.com>
2021-11-14 18:02:55 +0700

    feat((pages)): configure pages router and naming

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="h-105"></div>
+  <section>
+    <div class="pt-31 z-0 relative">
+      <p>Content</p>
+      <p>Content</p>
+      <p>Content</p>
+      <p>Content</p>
+      <p>Content</p>
+      <p>Content</p>
+      <p>Content</p>
+    </div>
+  </section>
 </template>
 
 <script>
