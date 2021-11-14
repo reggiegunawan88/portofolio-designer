1e3fba93669d516352c7beead358c8d07f490773
reggiegunawan <reggiegunawan88@gmail.com>
2021-11-14 18:35:34 +0700

    feat((home)): create portfolio work menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,4 @@
-<template>
-  <section>
-    <div class="pt-31 z-0 relative">
-      <p>Content</p>
-      <p>Content</p>
-      <p>Content</p>
-      <p>Content</p>
-      <p>Content</p>
-      <p>Content</p>
-      <p>Content</p>
-    </div>
-  </section>
-</template>
+<template src="./template.html"></template>
 
 <script>
 export default {}
